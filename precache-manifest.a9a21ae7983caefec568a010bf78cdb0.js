661aced351837a87457942597a75c53b32383565
Kaija Dunklin <kaykay_s@yahoo.com>
2020-07-30 09:37:28 -0400

    Updates

---
+++ precache-manifest.a9a21ae7983caefec568a010bf78cdb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9908906d8e07bc99217525826d2edde4",
+    "url": "/weekSix-DayTwo/index.html"
+  },
+  {
+    "revision": "f78075c3cd87cfa2edc4",
+    "url": "/weekSix-DayTwo/static/css/main.9d10fb6b.chunk.css"
+  },
+  {
+    "revision": "6da5443e80c362301f3b",
+    "url": "/weekSix-DayTwo/static/js/2.c9f8274d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weekSix-DayTwo/static/js/2.c9f8274d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78075c3cd87cfa2edc4",
+    "url": "/weekSix-DayTwo/static/js/main.02a6d020.chunk.js"
+  },
+  {
+    "revision": "3d63d7cdb1c3f37701e8",
+    "url": "/weekSix-DayTwo/static/js/runtime-main.f2a7e648.js"
+  }
+]);
